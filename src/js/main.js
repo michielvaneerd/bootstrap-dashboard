93410ae72c75d0407b322e45380c9829cebd5ebf
Michiel van Eerd <michiel.van.eerd@peercode.nl>
2024-03-21 10:09:10 +0100

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// https://github.com/twbs/examples/tree/main/vite
+// https://getbootstrap.com/docs/5.3/examples/dashboard/
+// https://icons.getbootstrap.com/#usage ==> Dus kopieer SVG en plak deze in de HTML en refereer eraan met xlink...
+
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+document.getElementById('my-main-nav-toggler').addEventListener('click', function() {
+    document.getElementById('my-main-nav').classList.toggle('my-main-nav-hidden');
+});
